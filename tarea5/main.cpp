e719bdae614b4ec0739c6a10eb531b545f58045e
Jose Antonio <joseantonio@MacBook-Pro-de-Jose.local>
2018-02-27 18:12:50 -0600

    Initial Commit

---
+++ tarea5/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  tarea5
+//
+//  Created by Jose Antonio on 27/02/18.
+//  Copyright © 2018 Jose Antonio. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
